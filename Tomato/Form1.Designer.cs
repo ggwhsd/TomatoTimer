c61958b9d49ce13f1a5f30a87653c5d03dd98164
gugw <ggw_hsd@hotmail.com>
2019-10-09 14:26:49 +0800

    first commit

---
+++ Tomato/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Tomato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_task = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.badTomato_counts = new System.Windows.Forms.TextBox();
+            this.goodTomato_counts = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_details = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_task
+            // 
+            this.textBox_task.Location = new System.Drawing.Point(144, 34);
+            this.textBox_task.Name = "textBox_task";
+            this.textBox_task.Size = new System.Drawing.Size(145, 21);
+            this.textBox_task.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "当前工作";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Red;
+            this.progressBar1.Location = new System.Drawing.Point(144, 61);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(145, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "剩余时间";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(47, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "放弃";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(97, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.badTomato_counts);
+            this.groupBox1.Controls.Add(this.goodTomato_counts);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(39, 183);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 72);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "专注统计";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "坏番茄数";
+            // 
+            // badTomato_counts
+            // 
+            this.badTomato_counts.Location = new System.Drawing.Point(76, 36);
+            this.badTomato_counts.Name = "badTomato_counts";
+            this.badTomato_counts.Size = new System.Drawing.Size(25, 21);
+            this.badTomato_counts.TabIndex = 1;
+            // 
+            // goodTomato_counts
+            // 
+            this.goodTomato_counts.Location = new System.Drawing.Point(76, 14);
+            this.goodTomato_counts.Name = "goodTomato_counts";
+            this.goodTomato_counts.Size = new System.Drawing.Size(25, 21);
+            this.goodTomato_counts.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "好番茄数";
+            // 
+            // textBox_details
+            // 
+            this.textBox_details.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_details.Location = new System.Drawing.Point(295, 29);
+            this.textBox_details.Name = "textBox_details";
+            this.textBox_details.Size = new System.Drawing.Size(220, 226);
+            this.textBox_details.TabIndex = 8;
+            this.textBox_details.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "完成";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 263);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_details);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_task);
+            this.Name = "Form1";
+            this.Text = "番茄时间工作法";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_task;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox badTomato_counts;
+        private System.Windows.Forms.TextBox goodTomato_counts;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox textBox_details;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Tomato/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tomato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private enum TomatoStatus
+        {
+            None,
+            EatingTomato,
+            HaveRest,
+            HaveLongRest
+
+        }
+        TomatoStatus DoingStatus;
+        private int RestCount = 0;
+        private int goodTomato = 0;
+        private int badTomato = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Step = 1;
+            switch (DoingStatus)
+            {
+                case TomatoStatus.None:
+
+                    switchTomato();
+                    timer1.Start();
+                    break;
+
+                case TomatoStatus.EatingTomato:
+                    {
+                        
+                        timer1.Start();
+                    }
+                    break;
+
+                case TomatoStatus.HaveRest:
+                    {
+                        RestCount += 1;
+                        timer1.Start();
+                    }
+                    break;
+
+                case TomatoStatus.HaveLongRest:
+                    {
+                        timer1.Start();
+                    }
+                    break;
+
+                default:
+                    break;
+             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (DoingStatus != TomatoStatus.None) {
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value += progressBar1.Step;
+                    label3.Text = string.Format("{0:D2}", ((progressBar1.Maximum - progressBar1.Value) / 60).ToString()) + ":" + string.Format("{0:D2}", ((progressBar1.Maximum - progressBar1.Value) % 60));
+                }
+                else 
+                {
+                    timer1.Stop();
+                    if (DoingStatus == TomatoStatus.EatingTomato)
+                    {
+                        UpdateLog("完成一个番茄时间");
+                        goodTomato++;
+                        if (RestCount % 2 == 0 && RestCount != 0)
+                        {
+                            switchLongRest();
+                        }
+                        else
+                        {
+                            switchShortRest();
+                        }
+
+                    }
+                    else if (DoingStatus == TomatoStatus.HaveRest)
+                    {
+                        UpdateLog("完成5分钟休息");
+                        switchTomato();
+                    }
+                    else if (DoingStatus == TomatoStatus.HaveLongRest)
+                    {
+                        UpdateLog("完成15分钟休息");
+                        switchTomato();
+                    }
+                    
+                }
+            }
+            UpdateStatistic();
+
+
+        }
+        private void UpdateStatistic()
+        {
+            goodTomato_counts.Text = goodTomato.ToString();
+            badTomato_counts.Text = badTomato.ToString();
+
+        }
+
+        private void switchShortRest()
+        {
+            progressBar1.Maximum = 5*60 ;
+
+            progressBar1.Value = 0;
+            label3.Text = "05:00";
+            timer1.Interval = 1000;
+            button1.Text = "休息5分钟";
+            DoingStatus = TomatoStatus.HaveRest;
+            UpdateLog(button1.Text);
+        }
+
+        private void switchLongRest()
+        {
+            progressBar1.Maximum = 15*60 ;
+
+            progressBar1.Value = 0;
+            label3.Text = "15:00";
+            timer1.Interval = 1000;
+            button1.Text = "休息15分钟";
+            DoingStatus = TomatoStatus.HaveLongRest;
+            UpdateLog(button1.Text);
+        }
+        private void switchNone()
+        {
+            progressBar1.Value = 0;
+            label3.Text = "";
+            timer1.Interval = 1000;
+            button1.Text = "开始";
+            DoingStatus = TomatoStatus.None;
+            UpdateLog("放弃");
+        }
+        private void switchTomato()
+        {
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 25*60;
+ 
+            progressBar1.Value = 0;
+            label3.Text = "25:00";
+            timer1.Interval = 1000;
+
+            button1.Text = "专注";
+            RestCount = 0;
+            DoingStatus = TomatoStatus.EatingTomato;
+            UpdateLog(button1.Text);
+
+        }
+        private void switchFinishTask()
+        {
+            if (DoingStatus == TomatoStatus.EatingTomato)
+            {
+                goodTomato++;
+            }
+            progressBar1.Value = 0;
+            label3.Text = "";
+            timer1.Interval = 1000;
+            button1.Text = "开始";
+            DoingStatus = TomatoStatus.None;
+            UpdateLog("任务【" + textBox_task.Text + "】完成");
+            textBox_task.Text = "";
+            
+        }
+        private void UpdateLog(string msg)
+        {
+            textBox_details.Text += DateTime.Now.ToShortTimeString() + " " + msg + "\n";
+        }
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            switchNone();
+            badTomato++;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            switchFinishTask();
+            
+            
+        }
+    }
+}
